b7b998be1192c5ad91af54d873a9eba5d9b63fbf
asrawat <rawat.anoop81@gmail.com>
2020-07-30 12:57:40 +0530

    Added new function vc_open
    Added function vc_open which opens veracrypt directly from within R

---
+++ R/vc_open.R
@@ -0,0 +1,21 @@
+#' Opens veracrypt container directly from R
+#'
+#' This function opens the veracrypt container directly from R
+#' @param filename character: name of the veracrypt container. If it doesn't contain an absolute path, the file name is relative to the current working directory, getwd().
+#' @param drive character: drive name. Example - "Z://"
+#' @param password character: password of the veracrypt container.
+#' @param vcexecutable character: name of the veracrypt executable file. Default is "C:/Program Files/VeraCrypt/VeraCrypt.exe"
+#' @return Running the function will open veracrypt as specified. Returns a NULL object in R.
+#' @export
+
+vc_open<-function(filename,drive,password,vcexecutable="C:\\Program Files\\VeraCrypt\\VeraCrypt.exe") {
+  if(dir.exists(drive)) {
+    e<-base::simpleError(paste("Drive",drive, "is not available. Either unmount the volume from this drive or select an available drive"))
+    stop(e)
+  } else {
+    system(paste("\"",vcexecutable,"\"", ' /v ' , "\"" ,filename,"\"",' /l ', drive,' /a /p ', password,' /e /b',sep = ""),wait = F)
+    while(!dir.exists(drive)) {
+
+    }
+  }
+}
